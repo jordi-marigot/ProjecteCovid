6b3f750b45d39c06f8b9d83ddfd0d28be3aa8693
incognitoPeroNo <98736519+incognitoPeroNo@users.noreply.github.com>
2022-02-08 12:38:48 +0100

    Update projecteCovidJordiMArseny.java

--- projecteCovidJordiMArseny.java
+++ projecteCovidJordiMArseny.java
@@ -4,9 +4,10 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Scanner;
 
-public class extreureCasosCovid {    
+public class projecteCovidJordiMArseny {    
     public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
+        double percentatge = 0.0;
 
         System.out.print("Indica el municipi que vols cercar: ");
         String nomMunicipi = s.nextLine().trim();
@@ -15,20 +16,27 @@ public class extreureCasosCovid {
         s.close();
 
         String[] respostaFuncioCodi = consultaCodiMunicipi(nomMunicipi, tipusCerca);
-        String codiMunicipi = respostaFuncioCodi[0]; // obtenir nomes el codiMunicipi, sense el nom.
-
-        System.out.println(codiMunicipi);
+        String codiMunicipi = respostaFuncioCodi[0];            // Obtenir nomes el codiMunicipi, sense el nom.
         
+        // Control de error per calcular el percentatge amb un valor null
         // Avisar al Usuari de un error de cerca del municipi indicat
         if(codiMunicipi != null){
-            System.out.printf("Num casos en el municipi %s: %d",respostaFuncioCodi[1],consultaCasosCovid(codiMunicipi));
+            System.out.printf("Num casos en el municipi %s: %d\n",respostaFuncioCodi[1],consultaCasosCovid(codiMunicipi));
+            // Calcular percentatge de casos covid per municipi
+            percentatge = ((double)consultaCasosCovid(codiMunicipi)/consultaHabitants(codiMunicipi))*100;
+            System.out.printf("El percentatge de contegiats per covid en el municipi %s es del: %.2f%%\n", respostaFuncioCodi[1], percentatge);
         }
         else{
-            System.out.println("No s'ha troat el municipi indicat!!");
+            System.out.println("No s'ha trobat el municipi indicat!!");
         }
         
-        
+        // Avis al Usuari si es retorna un 0.0% indicant que pot ser degut a manca de dades, etc...
+        // Tambe es omprova que el codi no sigui nul, ja que si ho es ja rebra una resposta diferent indicant que el codi no s'ha trobat
+        if(percentatge == 0 && codiMunicipi != null){
+            System.out.println("Aquest resultat pot ser degut, molt probablement, a una manca de dades per secret estadístic, més que no pas a un registre oficial de 0 casos (això passa amb els municipis que tenen molt pocs habitants)");
+        }
     }
+
     /**
      * Funció que es connecta a l'API "Unitats poblacionals Catalunya" del portal  
      * "analisi.transparenciacatalunya.cat" per consultar el codi IDESCAT d'un
@@ -51,6 +59,7 @@ public class extreureCasosCovid {
      *                     cap municipi català que compleixi les condicions de cerca,
      *                     l'array retornat valdrà null. Si algun dels dos paràmetres
      *                     val nul o és buit, també es retornarà null.
+     * 
      *                     Aixi que mostrara un missatge indicat que no s'ha trobat 
      *                     el municipi indicat.
      *                             
@@ -157,7 +166,6 @@ public class extreureCasosCovid {
             // prevenir la primera linia ja que es la dels noms dels camps
             if (a > 0){
                 numCasos += extreureNumeroCasosCOVID(liniaQCSV);
-                //System.out.println(extreureNumeroCasosCOVID(liniaQCSV));
             }
         }
             // Per detectar algun error en la cerca o que els valors rebuts son nuls, fem un filtre per indicar -1 en algun dels casos
@@ -183,11 +191,82 @@ public class extreureCasosCovid {
 	 */
     public static int extreureNumeroCasosCOVID(String liniaQCSV) {
         int totalCasos = 0;
-        String numeroCasos = extreureElementQCSV(liniaQCSV, 10);
-        totalCasos += Integer.parseInt(numeroCasos);
+        String numeroCasos = extreureElementQCSV(liniaQCSV, 10);    // Parametre 10 per extreure nomes la ultima columna on s'hindiquen el casos
+        totalCasos += Integer.parseInt(numeroCasos);                // Convertim a Int cada fila retornada per poder sumar les diferentes files i no concadenar-les
 
         return totalCasos;
+    }
+    
+        /**
+     * Funció que es connecta a l'API "Població de Catalunya per municipi, rang
+     * d'edat i sexe" del portal "analisi.transparenciacatalunya.cat" per
+     * consultar el nombre total d'habitants d'un municipi concret de Catalunya,
+     * en el padró del 2020, durant el qual s'ha produït, per ara, el gruix
+     * principal de la pandèmia.
+     * 
+     * @param codiIdescatMunicipi Cadena de caràcters que conté el codi identificatiu
+     *                            del municipi utilitzat per l'IDESCAT (l'Institut
+     *                            d'Estadística de Catalunya).
+     *
+     * @return                      Nombre d'habitants registrats en el municipi
+     *                               indicat pel codi IDESCAT del paràmetre d'entrada
+     *                               en el padró de l'any 2020.
+     *                            Si el codi que es passa és nul o buit, o es tracta
+     *                            d'un codi de municipi inexistent, el resultat
+     *                            retornat serà de -1.
+     *
+     * @throws IOException        La funció pot generar errors d'entrada/sortida no
+     *                            controlats, ja que el tema de gestió d'errors i
+     *                            excepcions no s'introdueix fins a la UF-5.
+     */
+    public static int consultaHabitants(String codiIdescatMunicipi) throws IOException {
+        URL urlHabitants;
+        int numHabitants = 0;
+
+        // Creem un Scanner, associant-lo a l'objecte sURL que acabarem creant.
+        Scanner sURL;
+        urlHabitants = new URL("https://analisi.transparenciacatalunya.cat/resource/b4rr-d25b.csv?any=2020&codi="+codiIdescatMunicipi+"");
+        sURL = new Scanner(new InputStreamReader(urlHabitants.openStream(), "UTF-8"));
+
+        // Bucle per enviar linia per linia a "extreureNumeroHabitants" exeptuant la linia 0 ja que no te valors necessaris per el programa
+        for (int a = 0; sURL.hasNext(); a++) {
+            String liniaQCSV = sURL.nextLine();
+            if (a > 0){
+                numHabitants = extreureNumeroHabitants(liniaQCSV);
+            }
+        }   
+        return numHabitants;
+	}
+    	/**
+	 * Funció que, donanda una línia QCSV obtinguda com a resultat d'una consulta
+	 * a l'API "Població de Catalunya per municipi, rang d'edat i sexe" del
+	 * portal "analisi.transparenciacatalunya.cat", ens retorna la suma dels
+	 * elements que ens indiquen el nombre d'habitants d'un municipi.
+	 * 
+	 * @param liniaQCSV             Cadena de caràcters que conté la línia QCSV amb les dades
+	 *                              del registre de dades obtingut.
+	 *                  
+	 * @return sumaHabitants        Nombre d'habitants del municipi al qual correspon el registre
+	 *                              de dades QCSV que ens han passat com a paràmetre. En cas 
+	 *                              d'error (el registre no té l'estructura correcta, o la
+	 *                              cadena rebuda és nul·la o buida), es retorna 0 per no
+	 *                              interferir en el resultat d'un hipotètic recompte d'habitants.  
+	 */
+	public static int extreureNumeroHabitants(String liniaQCSV) {
+        String numeroHabitants = "";
+        int sumaHabitants = 0;
+
+        // Bucle per solicitar quines columnes es volen consultar
+        // La variable "i" es comença a partir del 9 ja que la primera columna que volem consultar esta ubicada alla
+        int i = 9;      
+        while(i < 12){
+            numeroHabitants = extreureElementQCSV(liniaQCSV, i);
+            sumaHabitants += Integer.parseInt(numeroHabitants);
+            i++;
         }
+        return sumaHabitants;
+    }
+    
     	/**
 	 * Funció que extreu un determinat element d'una cadena que conté un registre
 	 * d'informació en format Quoted CSV.
@@ -197,11 +276,11 @@ public class extreureCasosCovid {
 	 *                  
 	 * @param numElem   Número d'element que volem extreure de la cadena QCSV.
 	 * 
-	 * @return Valor de l'element demanat, havent-li eliminat les cometes que
-	 *         en delimiten el seu contingut. Si l'element que es demana no és vàlid
-	 *         perquè ens donen un nombre menor o igual que 0, o un nombre més gran 
-	 *         que la quantitat d'elements que té la cadena QCSV, o perquè la 
-	 *         cadena QCSV és nul·la o buida, el resultat que es retornarà és null.
+	 * @return resultat Valor de l'element demanat, havent-li eliminat les cometes que
+	 *                  en delimiten el seu contingut. Si l'element que es demana no és vàlid
+	 *                  perquè ens donen un nombre menor o igual que 0, o un nombre més gran 
+	 *                  que la quantitat d'elements que té la cadena QCSV, o perquè la 
+	 *                  cadena QCSV és nul·la o buida, el resultat que es retornarà és null.
 	 */
 	public static String extreureElementQCSV(String liniaQCSV, int numElem) {
         liniaQCSV=liniaQCSV.replace(",,", ",\"*\",");  // Les lines QCSV  de casos covid poden tenir ,, indicat que es una columna buida, la omplirem amb un * per buscar coma per coma sense trobar dos de juntes
